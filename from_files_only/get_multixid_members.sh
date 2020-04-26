4464644104c23818ff28698920f0cb2fc724e544
bdt <bdrouvot@gmail.com>
2020-04-26 08:31:39 +0000

    block size

--- from_files_only/get_multixid_members.sh
+++ from_files_only/get_multixid_members.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "$1" = "--help" ] || [ $# -ne 4 ] ; then
+if [ "$1" = "--help" ] || [ $# -lt 4 ] ; then
   echo ""
   echo "Usage: "
   echo ""
-  echo "-m: mxid"
-  echo "-d: DATA path"
+  echo "-m: mxid (mandatory)"
+  echo "-d: DATA path (mandatory)"
+  echo "-b: block size (default 8192)"
   echo ""
   echo "example: ./get_multixid_members.sh -m 2 -d /usr/local/pgsql11.6/data/"
   exit 0
@@ -27,6 +28,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -b|--blcks)
+    blcks="$2"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -36,9 +42,12 @@ done
 
 set -- "${POSITIONAL[@]}"
 
+blcks=${blcks:-8192}
+
 echo ""
 echo "MXID   = ${mxid}"
 echo "DATA   = ${pgdata}"
+echo "BLCKS  = ${blcks}"
 echo ""
 
 #See multixact.c:
@@ -54,26 +63,38 @@ echo ""
 OFFLOC="${pgdata}/pg_multixact/offsets/"
 MEMBLOC="${pgdata}/pg_multixact/members/"
 
+#SLRU_PAGES_PER_SEGMENT is 32
+slru_pages_per_segment=32
+
+#define MULTIXACT_MEMBERGROUPS_PER_PAGE (BLCKSZ / MULTIXACT_MEMBERGROUP_SIZE)
+multixact_membergroups_per_page=`expr ${blcks} / 20`
+
+#define MULTIXACT_MEMBERS_PER_PAGE (MULTIXACT_MEMBERGROUPS_PER_PAGE * MULTIXACT_MEMBERS_PER_MEMBERGROUP)
+multixact_members_per_page=`expr ${multixact_membergroups_per_page} \* 4`
+
+#define MULTIXACT_OFFSETS_PER_PAGE (BLCKSZ / sizeof(MultiXactOffset))
+multixact_offsets_per_page=`expr ${blcks} / 4`
+
 #define MultiXactIdToOffsetPage(xid) \ ((xid) / (MultiXactOffset) MULTIXACT_OFFSETS_PER_PAGE)
-pagenumber=`expr ${mxid} / 2048`
-pageinfile=`expr ${pagenumber} % 32`
+pagenumber=`expr ${mxid} / ${multixact_offsets_per_page}`
+pageinfile=`expr ${pagenumber} % ${slru_pages_per_segment}`
 
 #define MultiXactIdToOffsetSegment(xid) (MultiXactIdToOffsetPage(xid) / SLRU_PAGES_PER_SEGMENT)
-hexfic=`expr ${pagenumber} / 32`
+hexfic=`expr ${pagenumber} / ${slru_pages_per_segment}`
 hexfic=`echo "ibase=10;obase=16;${hexfic}" | bc`
 hexfic=`printf "%04x" "0x${hexfic}" | tr '[:lower:]' '[:upper:]'`
 
 #define MultiXactIdToOffsetEntry(xid) \ ((xid) % (MultiXactOffset) MULTIXACT_OFFSETS_PER_PAGE)
-offsetid=`expr ${mxid} % 2048`
+offsetid=`expr ${mxid} % ${multixact_offsets_per_page}`
 
 # Same computation with next mxid
 nextmxid=`expr ${mxid} + 1`
-nextpagenumber=`expr ${nextmxid} / 2048`
-nextpageinfile=`expr ${nextpagenumber} % 32`
-nexthexfic=`expr ${nextpagenumber} / 32`
+nextpagenumber=`expr ${nextmxid} / ${multixact_offsets_per_page}`
+nextpageinfile=`expr ${nextpagenumber} % ${slru_pages_per_segment}`
+nexthexfic=`expr ${nextpagenumber} / ${slru_pages_per_segment}`
 nexthexfic=`echo "ibase=10;obase=16;${nexthexfic}" | bc`
 nexthexfic=`printf "%04x" "0x${nexthexfic}" | tr '[:lower:]' '[:upper:]'`
-nextoffsetid=`expr ${nextmxid} % 2048`
+nextoffsetid=`expr ${nextmxid} % ${multixact_offsets_per_page}`
 
 #echo "Looking for offset $offsetid in page $pagenumber"
 #echo "Page $pagenumber is page $pageinfile in file ${OFFLOC}${hexfic}"
@@ -83,8 +104,8 @@ nextoffsetid=`expr ${nextmxid} % 2048`
 skipread=`expr ${offsetid} \* 4`
 next_skipread=`expr ${nextoffsetid} \* 4`
 
-offset_members=`dd status=none bs=8192 count=1 if=${OFFLOC}${hexfic} skip=$pageinfile | od -A n -t u4 -j $skipread -N 4 | sed 's/^ *//'`
-next_offset_members=`dd status=none bs=8192 count=1 if=${OFFLOC}${nexthexfic} skip=$nextpageinfile | od -A n -t u4 -j $next_skipread -N 4 | sed 's/^ *//'`
+offset_members=`dd status=none bs=${blcks} count=1 if=${OFFLOC}${hexfic} skip=$pageinfile | od -A n -t u4 -j $skipread -N 4 | sed 's/^ *//'`
+next_offset_members=`dd status=none bs=${blcks} count=1 if=${OFFLOC}${nexthexfic} skip=$nextpageinfile | od -A n -t u4 -j $next_skipread -N 4 | sed 's/^ *//'`
 #echo "Offset value is: ${offset_members}"
 #echo "NextOffset value is: ${next_offset_members}"
 
@@ -101,22 +122,22 @@ then
   while [ $off -lt $next_offset_members ]
   do
   #define MXOffsetToMemberPage(xid) ((xid) / (TransactionId) MULTIXACT_MEMBERS_PER_PAGE)
-  memberpage=`expr ${off} / 1636`
-  memberpageinfile=`expr ${memberpage} % 32`
+  memberpage=`expr ${off} / ${multixact_members_per_page}`
+  memberpageinfile=`expr ${memberpage} % ${slru_pages_per_segment}`
 
   #define MXOffsetToMemberSegment(xid) (MXOffsetToMemberPage(xid) / SLRU_PAGES_PER_SEGMENT)
-  membersegment=`expr ${memberpage} / 32`
+  membersegment=`expr ${memberpage} / ${slru_pages_per_segment}`
   membersegment=`echo "ibase=10;obase=16;${membersegment}" | bc`
   membersegment=`printf "%04x" "0x${membersegment}" | tr '[:lower:]' '[:upper:]'`
 
   #define MXOffsetToFlagsOffset(xid) \ ((((xid) / (TransactionId) MULTIXACT_MEMBERS_PER_MEMBERGROUP) % (TransactionId) MULTIXACT_MEMBERGROUPS_PER_PAGE) * (TransactionId) MULTIXACT_MEMBERGROUP_SIZE)
-  flagtooffset=`echo "$(( (${off} / 4 ) % 409 * 20 ))"`
+  flagtooffset=`echo "$(( (${off} / 4 ) % ${multixact_membergroups_per_page} * 20 ))"`
 
   #define MXOffsetToMemberOffset(xid) \ (MXOffsetToFlagsOffset(xid) + MULTIXACT_FLAGBYTES_PER_GROUP + ((xid) % MULTIXACT_MEMBERS_PER_MEMBERGROUP) * sizeof(TransactionId))
   memberoffset=`echo "$(( ${flagtooffset} + 4 + ( ${off} % 4) * 4 ))"`
 
   #echo "Reading offset $memberoffset in page $memberpageinfile in file ${MEMBLOC}${membersegment}"
-  dd status=none bs=8192 count=1 if=${MEMBLOC}${membersegment} skip=$memberpageinfile | od -A n -t u4 -j $memberoffset -N 4 | sed 's/^ *//'
+  dd status=none bs=${blcks} count=1 if=${MEMBLOC}${membersegment} skip=$memberpageinfile | od -A n -t u4 -j $memberoffset -N 4 | sed 's/^ *//'
   off=$(( $off + 1 ))
   done
 
@@ -125,22 +146,22 @@ else
   while $nextone
   do
   #define MXOffsetToMemberPage(xid) ((xid) / (TransactionId) MULTIXACT_MEMBERS_PER_PAGE)
-  memberpage=`expr ${off} / 1636`
-  memberpageinfile=`expr ${memberpage} % 32`
+  memberpage=`expr ${off} / ${multixact_members_per_page}`
+  memberpageinfile=`expr ${memberpage} % ${slru_pages_per_segment}`
 
   #define MXOffsetToMemberSegment(xid) (MXOffsetToMemberPage(xid) / SLRU_PAGES_PER_SEGMENT)
-  membersegment=`expr ${memberpage} / 32`
+  membersegment=`expr ${memberpage} / ${slru_pages_per_segment}`
   membersegment=`echo "ibase=10;obase=16;${membersegment}" | bc`
   membersegment=`printf "%04x" "0x${membersegment}" | tr '[:lower:]' '[:upper:]'`
 
   #define MXOffsetToFlagsOffset(xid) \ ((((xid) / (TransactionId) MULTIXACT_MEMBERS_PER_MEMBERGROUP) % (TransactionId) MULTIXACT_MEMBERGROUPS_PER_PAGE) * (TransactionId) MULTIXACT_MEMBERGROUP_SIZE)
-  flagtooffset=`echo "$(( (${off} / 4 ) % 409 * 20 ))"`
+  flagtooffset=`echo "$(( (${off} / 4 ) % ${multixact_membergroups_per_page} * 20 ))"`
 
   #define MXOffsetToMemberOffset(xid) \ (MXOffsetToFlagsOffset(xid) + MULTIXACT_FLAGBYTES_PER_GROUP + ((xid) % MULTIXACT_MEMBERS_PER_MEMBERGROUP) * sizeof(TransactionId))
   memberoffset=`echo "$(( ${flagtooffset} + 4 + ( ${off} % 4) * 4 ))"`
 
   #echo "Reading offset $memberoffset in page $memberpageinfile in file ${MEMBLOC}${membersegment}"
-  THISMEMB=`dd status=none bs=8192 count=1 if=${MEMBLOC}${membersegment} skip=$memberpageinfile | od -A n -t u4 -j $memberoffset -N 4 | sed 's/^ *//'`
+  THISMEMB=`dd status=none bs=${blcks} count=1 if=${MEMBLOC}${membersegment} skip=$memberpageinfile | od -A n -t u4 -j $memberoffset -N 4 | sed 's/^ *//'`
   if [ $THISMEMB -ne 0 ]
     then
     echo "${THISMEMB}"
--- from_files_only/get_xact_status.sh
+++ from_files_only/get_xact_status.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-if [ "$1" = "--help" ] || [ $# -ne 4 ] ; then
+if [ "$1" = "--help" ] || [ $# -lt 4 ] ; then
   echo ""
   echo "Usage: "
   echo ""
   echo "-x: xid"
   echo "-d: DATA path"
+  echo "-b: block size (default 8192)"
   echo ""
   echo "example: $0 -x 31795287 -d /usr/local/pgsql11.6/data/"
   exit 0
@@ -27,6 +28,11 @@ case $key in
     shift # past argument
     shift # past value
     ;;
+    -b|--blcks)
+    blcks="$2"
+    shift # past argument
+    shift # past value
+    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift # past argument
@@ -36,9 +42,12 @@ done
 
 set -- "${POSITIONAL[@]}"
 
+blcks=${blcks:-8192}
+
 echo ""
 echo "XID   = ${xid}"
 echo "DATA  = ${pgdata}"
+echo "BLCKS = ${blcks}"
 echo ""
 
 #See clog.c:
@@ -46,23 +55,24 @@ echo ""
 #define CLOG_XACTS_PER_BYTE 4
 #define CLOG_XACTS_PER_PAGE (BLCKSZ * CLOG_XACTS_PER_BYTE)
 
-# Assuming BLCKSZ is 8k
-#CLOG_BITS_PER_XACT      2
-#CLOG_XACTS_PER_BYTE 4
-#CLOG_XACTS_PER_PAGE 32768
-
 PGCLOG="${pgdata}/pg_xact/"
 
-#define TransactionIdToPage(xid)    ((xid) / (TransactionId) CLOG_XACTS_PER_PAGE)
-pagenumber=`expr ${xid} / 32768`
-pageinfile=`expr ${pagenumber} % 32`
+#SLRU_PAGES_PER_SEGMENT is 32
+slru_pages_per_segment=32
 
-hexfic=`expr ${pagenumber} / 32`
+#define CLOG_XACTS_PER_PAGE (BLCKSZ * CLOG_XACTS_PER_BYTE)
+clog_xacts_per_page=`expr ${blcks} \* 4`
+
+#define TransactionIdToPage(xid)    ((xid) / (TransactionId) CLOG_XACTS_PER_PAGE)
+pagenumber=`expr ${xid} / ${clog_xacts_per_page}`
+pageinfile=`expr ${pagenumber} % ${slru_pages_per_segment}`
+
+hexfic=`expr ${pagenumber} / ${slru_pages_per_segment}`
 hexfic=`echo "ibase=10;obase=16;${hexfic}" | bc`
 hexfic=`printf "%04x" "0x${hexfic}" | tr '[:lower:]' '[:upper:]'`
 
 #define TransactionIdToPgIndex(xid) ((xid) % (TransactionId) CLOG_XACTS_PER_PAGE)
-offsetid=`expr ${xid} % 32768`
+offsetid=`expr ${xid} % ${clog_xacts_per_page}`
 
 #define TransactionIdToByte(xid)    (TransactionIdToPgIndex(xid) / CLOG_XACTS_PER_BYTE)
 xidtobyte=`expr ${offsetid} / 4`
@@ -70,7 +80,7 @@ xidtobyte=`expr ${offsetid} / 4`
 #define TransactionIdToBIndex(xid)  ((xid) % (TransactionId) CLOG_XACTS_PER_BYTE)
 xidtobindex=`expr ${xid} % 4`
 
-BYREAD=`dd status=none bs=8192 count=1 if=${PGCLOG}${hexfic} skip=$pageinfile | od -A n -t x1 -j $xidtobyte -N 1| sed 's/^ *//'`
+BYREAD=`dd status=none bs=${blcks} count=1 if=${PGCLOG}${hexfic} skip=$pageinfile | od -A n -t x1 -j $xidtobyte -N 1| sed 's/^ *//'`
 BYREAD=`echo $BYREAD | tr '[:lower:]' '[:upper:]'`
 ALLBITS=`echo "ibase=16;obase=2;${BYREAD}" | bc`
 NBBITS=`echo -n "$ALLBITS" | wc -c`
