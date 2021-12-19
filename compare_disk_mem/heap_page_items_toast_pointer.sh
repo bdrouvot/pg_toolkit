7249a6c1f8e6cb5f5d50fa2c4b17113a0ba8bfa3
bdrouvot <bdrouvot@gmail.com>
2021-12-19 12:16:34 +0000

    display xmin and xmax with od as u4 instead of d4

--- compare_disk_mem/heap_page_items_toast_pointer.sh
+++ compare_disk_mem/heap_page_items_toast_pointer.sh
@@ -84,9 +84,9 @@ do
   dsk_lp_len=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t x2 -j $skipbytes -N 2 | sed 's/^ *//'`
   dsk_lp_len=`echo $((0x$dsk_lp_len >> 1))`
   dsk_lp_flags="$((2#`echo "ibase=16;obase=2;${dsk_lp_flags}" | bc | rev | cut -c16,17 | rev`))"
-  dsk_xmin=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t d4 -j $dsk_lp_off -N 4`
+  dsk_xmin=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t u4 -j $dsk_lp_off -N 4`
   toread=$(( 4 + $dsk_lp_off ))
-  dsk_xmax=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t d4 -j $toread -N 4`
+  dsk_xmax=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t u4 -j $toread -N 4`
   toread=$(( 8 + $dsk_lp_off ))
   dsk_t_field3=`dd status=none bs=8192 count=1 if=$relpath skip=$blk | od -A n -t u4 -j $toread -N 4`
   toread=$(( 12 + $dsk_lp_off ))
